3eaf8263f6aaa49940ec16070e6a4ed0a3aa3fee
Ethel <134110862+ethelbeluzzi@users.noreply.github.com>
2025-08-15 17:15:27 -0300

    Update inicio.py

--- paginas/inicio.py
+++ paginas/inicio.py
@@ -27,13 +27,22 @@ def pagina_inicio():
             df_disciplina = df[df["disciplina"] == disciplina]
             df_disciplina = df_disciplina.sort_values(by=["unidade", "aula"])
 
+            cols = st.columns(4)  # cria primeira linha de 4 colunas
+            col_index = 0
+
             for _, row in df_disciplina.iterrows():
                 unidade = extrair_numero(row["unidade"])
                 aula = extrair_numero(row["aula"])
 
-                if st.button(
-                    f"Unidade {unidade} | Aula {aula}",
-                    key=f"{disciplina}_u{unidade}_a{aula}"
-                ):
-                    st.session_state.pagina = f"disciplina_{disciplina}_u{unidade}_a{aula}"
-                    st.rerun()
+                with cols[col_index]:
+                    if st.button(
+                        f"Unidade {unidade} | Aula {aula}",
+                        key=f"{disciplina}_u{unidade}_a{aula}"
+                    ):
+                        st.session_state.pagina = f"disciplina_{disciplina}_u{unidade}_a{aula}"
+                        st.rerun()
+
+                col_index += 1
+                if col_index == 4:
+                    cols = st.columns(4)  # reinicia nova linha de 4 colunas
+                    col_index = 0
